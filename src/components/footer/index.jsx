581c845820c523a1813008286f2aba6292a65509
unknown <milan_cirkovic@yahoo.com>
2020-11-06 23:14:29 +0100

    editing footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import "./style.css"
 import Button from "../button"
+import { Link } from "gatsby"
 
 function Footer() {
   return (
@@ -22,6 +23,33 @@ function Footer() {
             />
             <Button buttonStyle="btn--outline">Subcribe</Button>
           </form>
+        </div>
+      </section>
+      <div className="footer-links">
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
+            <h2>About Us</h2>
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/testimonials">Testimonials</Link>
+          </div>
+        </div>
+      </div>
+      <section className="social-media">
+        <div className="social-media-wrap">
+          <div className="footer-logo">
+            <Link to="/" className="social-logo">
+              import logo!!!
+            </Link>
+          </div>
+          <small className="website-rights">subudei © 2020</small>
+          <Link
+            className="social-icon-link facebook"
+            to="/"
+            target="_blank"
+            aria-label="Facebook"
+          >
+            F
+          </Link>
         </div>
       </section>
     </div>
